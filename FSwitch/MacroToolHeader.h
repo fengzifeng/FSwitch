93ca181ea0908b2521899204e239893694545fd5
fengzifeng <13011289657@163.com>
2016-06-12 17:10:38 +0800

    switch
    switch first

---
+++ FSwitch/MacroToolHeader.h
@@ -0,0 +1,75 @@
+//
+//  MacroToolHeader.h
+//  HLMagic
+//
+//  Created by chen ying on 13-10-6.
+//  Copyright (c) 2013年 chen ying. All rights reserved.
+//
+
+
+#ifndef HLMagic_MacroToolHeader_h
+#define HLMagic_MacroToolHeader_h
+
+//#ifdef DEBUG
+//#define FLOG(fmt,...)    NSLog((@"[%@][%d] " fmt),[[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__,##__VA_ARGS__)
+//#else
+//#define FLOG(str, args...) ((void)0)
+//#endif
+
+
+#define IS_IPAD              (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE            (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+
+#define SCREEN_HEIGHT        ([[UIScreen mainScreen] bounds].size.height)
+#define SCREEN_WIDTH         ([[UIScreen mainScreen] bounds].size.width)
+#define WINDOW_MAX_LENGTH    (MAX(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+#define IS_IPHONE_4          (IS_IPHONE && WINDOW_MAX_LENGTH < 568.0)
+#define IS_IPHONE_5          (IS_IPHONE && WINDOW_MAX_LENGTH == 568.0)
+#define IS_IPHONE_6          (IS_IPHONE && WINDOW_MAX_LENGTH == 667.0)
+#define IS_IPHONE_6P         (IS_IPHONE && WINDOW_MAX_LENGTH == 736.0)
+
+
+#define ShortSystemVersion   [[UIDevice currentDevice].systemVersion floatValue]
+#define IS_IOS_6             (ShortSystemVersion < 7)
+#define IS_IOS_7             (ShortSystemVersion >= 7 && ShortSystemVersion < 8)
+#define IS_IOS_8             (ShortSystemVersion >= 8)
+
+#define radians(degrees)     ((degrees)*M_PI/180.0f)
+
+
+#define userDefaults        [NSUserDefaults standardUserDefaults]
+#define KeyWindow           [[[UIApplication sharedApplication] delegate] window]
+#define WindowZoomScale     (WINDOW_WIDTH/320.f)
+#define UniversalZoomScale  (MIN(1.8, WindowZoomScale))  //适配iPad
+
+#define DocumentPath        [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject]    //获取Document文件夹的路径
+#define ResourcePath        [[NSBundle mainBundle] resourcePath]    //获取自定义文件的bundle路径
+#define ImageNamed(name)    [UIImage imageWithContentsOfFile:[ResourcePath stringByAppendingPathComponent:name]]
+#define RGBCOLOR(r,g,b)     [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]         //RGB进制颜色值
+#define RGBACOLOR(r,g,b,a)  [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]       //RGBA进制颜色值
+#define HexColor(hexValue)  [UIColor colorWithRed:((float)(((hexValue) & 0xFF0000) >> 16))/255.0 green:((float)(((hexValue) & 0xFF00) >> 8))/255.0 blue:((float)((hexValue) & 0xFF))/255.0 alpha:1]   //16进制颜色值，如：#000000 , 注意：在使用的时候hexValue写成：0x000000
+
+#define ThemColor            HexColor(0x096096)
+
+
+#define kFont_10 [UIFont systemFontOfSize:10]
+#define kFont_11 [UIFont systemFontOfSize:11]
+#define kFont_12 [UIFont systemFontOfSize:12]
+#define kFont_13 [UIFont systemFontOfSize:13]
+#define kFont_14 [UIFont systemFontOfSize:14]
+#define kFont_15 [UIFont systemFontOfSize:15]
+#define kFont_16 [UIFont systemFontOfSize:16]
+#define kFont_17 [UIFont systemFontOfSize:17]
+#define kFont_18 [UIFont systemFontOfSize:18]
+#define kFont_19 [UIFont systemFontOfSize:19]
+#define kFont_20 [UIFont systemFontOfSize:20]
+#define kFont_21 [UIFont systemFontOfSize:21]
+
+//获取随机数
+#define Random(from, to) (int)(from + (arc4random() % (to - from + 1))); //+1,result is [from to]; else is [from, to)!!!!!!!
+#define ARC4RANDOM_MAX (0x100000000 * 20)
+
+
+
+#endif
--- FSwitch/ViewController.m
+++ FSwitch/ViewController.m
@@ -7,21 +7,83 @@
 //
 
 #import "ViewController.h"
+#import "SwitchView.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<UIScrollViewDelegate>
+{
+    NSArray *_sourseArray;
+    
+}
+@property (strong, nonatomic) UIScrollView *scrollView;
+@property (strong, nonatomic) SwitchView *switchView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _sourseArray = @[@"第一",@"第一个",@"第二",@"第二个",@"第三",@"第三个",@"第四",@"第四个",@"第五",@"第五个",@"第六"];
+    
+    [self.view addSubview:self.scrollView];
+    [self.view addSubview:self.switchView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIScrollView *)scrollView
+{
+    if (!_scrollView) {
+        _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0,44 + 64, SCREEN_WIDTH, SCREEN_HEIGHT - 44 - 64)];
+        _scrollView.delegate = self;
+        _scrollView.pagingEnabled = YES;
+        _scrollView.scrollsToTop = NO;
+        _scrollView.contentSize = CGSizeMake(SCREEN_WIDTH*_sourseArray.count, SCREEN_HEIGHT - 44 - 64);
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.showsVerticalScrollIndicator = NO;
+        
+        for (int i = 0; i<_sourseArray.count; i++) {
+            UIView *view = [[UIView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH*i, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 44 - 64)];
+            view.backgroundColor = i%2?[UIColor yellowColor]:[UIColor whiteColor];
+            
+            [_scrollView addSubview:view];
+        }
+    }
+    
+    return _scrollView;
 }
+
+- (SwitchView *)switchView
+{
+    if (!_switchView) {
+        _switchView = [[SwitchView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, 44)];
+        _switchView.titleArray = _sourseArray;
+        __weak typeof (self.scrollView)weakScrollView = self.scrollView;
+        
+        [_switchView setTapItemWithIndex:^(NSInteger index,BOOL animation){
+            [weakScrollView scrollRectToVisible:CGRectMake(index*weakScrollView.frame.size.width, 0.0, weakScrollView.frame.size.width,weakScrollView.frame.size.height) animated:animation];
+        }];
+    }
+    
+    return _switchView;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    
+    if (scrollView == self.scrollView) {
+        NSInteger page = roundf(scrollView.contentOffset.x / scrollView.frame.size.width);
+        page = MAX(page, 0);
+        page = MIN(page, _sourseArray.count - 1);
+        
+        [_switchView endMoveToIndex:page];
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+
 
 @end
